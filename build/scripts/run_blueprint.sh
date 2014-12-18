bac10278d15303a001d41a0a6983eebf70fa0959
Matt Aasted <aasted@twitch.tv>
2014-12-18 12:03:37 -0800

    Fix run script

--- build/scripts/run_blueprint.sh
+++ build/scripts/run_blueprint.sh
@@ -9,7 +9,6 @@ cd -- "$(dirname -- "$0")"
 eval "$(curl 169.254.169.254/latest/user-data/)"
 
 exec ./blueprint \
-  -url="${SCOOP_URL}" \
+  -scoopURL="${SCOOP_URL}" \
   -transformConfig="${CONFIG_DIR}/transforms_available.json" \
   -staticfiles="${SCIENCE_DIR}/nginx/html"
-
