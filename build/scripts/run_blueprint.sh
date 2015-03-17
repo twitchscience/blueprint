dcd5f563c6c10df0190a063cdeea7994323e26c7
Matt Aasted <aasted@twitch.tv>
2015-03-17 14:47:44 -0700

    Move config into s3

--- build/scripts/run_blueprint.sh
+++ build/scripts/run_blueprint.sh
@@ -5,8 +5,11 @@ SCIENCE_DIR="/opt/science"
 CONFIG_DIR="${SCIENCE_DIR}/blueprint/config"
 
 cd -- "$(dirname -- "$0")"
-
-eval "$(curl 169.254.169.254/latest/user-data/)"
+eval "$(curl --silent 169.254.169.254/latest/user-data/)"
+export HOST="$(curl --silent 169.254.169.254/latest/meta-data/hostname)"
+export CONFIG_PREFIX="s3://$S3_CONFIG_BUCKET/$VPC_SUBNET_TAG/$CLOUD_APP/$CLOUD_ENVIRONMENT"
+aws s3 cp --region us-west-2 "$CONFIG_PREFIX/conf.sh" conf.sh
+source conf.sh
 
 exec ./blueprint \
   -scoopURL="${SCOOP_URL}" \
--- build/scripts/run_schema_suggestor.sh
+++ build/scripts/run_schema_suggestor.sh
@@ -5,8 +5,11 @@ CONFIG_DIR="/opt/science/blueprint/config"
 SCIENCE_DIR="/opt/science"
 
 cd -- "$(dirname -- "$0")"
-
-eval "$(curl 169.254.169.254/latest/user-data/)"
+eval "$(curl --silent 169.254.169.254/latest/user-data/)"
+export HOST="$(curl --silent 169.254.169.254/latest/meta-data/hostname)"
+export CONFIG_PREFIX="s3://$S3_CONFIG_BUCKET/$VPC_SUBNET_TAG/$CLOUD_APP/$CLOUD_ENVIRONMENT"
+aws s3 cp --region us-west-2 "$CONFIG_PREFIX/conf.sh" conf.sh
+source conf.sh
 
 export GOMAXPROCS="2"
 
