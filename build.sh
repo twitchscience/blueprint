31453b4f516967184698e37fba56ac5d84b83e90
Aasted <maasted@6c4008b8d070.ant.amazon.com>
2015-09-03 12:34:34 -0700

    Remove godep restore, re-order transformer build

--- build.sh
+++ build.sh
@@ -13,6 +13,7 @@ SECURITY_GROUP=$6
 # I hate boolean args, but I'm not sure how to handle this.
 USE_PRIVATE_IP=${7:-"false"}
 
+sudo apt-get install -y pkg-config
 go get github.com/twitchscience/spade/transformer_dumper
 transformer_dumper -outFile build/config/transforms_available.json
 
@@ -20,7 +21,6 @@ export GOARCH=amd64
 export GOOS=linux
 export GOBIN="/tmp/${PROJECT}_build_$$"
 
-godep restore
 godep go vet ./... # go get golang.org/x/tools/cmd/vet
 godep go test -v ./...
 godep go build -v ./...
