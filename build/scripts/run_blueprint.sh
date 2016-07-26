010d45f408da3f799015453f46bfe7c8944f6f0b
Jack Gao <jackgao@twitch.tv>
2016-07-25 21:00:04 -0700

    Added new user-data system to Blueprint
    Summary: Updated Blueprint to have segregated CloudWatch logs
    
    Test Plan: Passed integration test.
    
    Reviewers: dwe
    
    Reviewed By: dwe
    
    Subscribers: #scieng
    
    Differential Revision: https://phabricator.internal.justin.tv/D3828

--- build/scripts/run_blueprint.sh
+++ build/scripts/run_blueprint.sh
@@ -5,7 +5,6 @@ SCIENCE_DIR="/opt/science"
 CONFIG_DIR="${SCIENCE_DIR}/blueprint/config"
 
 cd -- "$(dirname -- "$0")"
-eval "$(curl --silent 169.254.169.254/latest/user-data/)"
 export HOST="$(curl --silent 169.254.169.254/latest/meta-data/hostname)"
 export CONFIG_PREFIX="s3://$S3_CONFIG_BUCKET/$VPC_SUBNET_TAG/$CLOUD_APP/$CLOUD_ENVIRONMENT"
 aws s3 cp --region us-west-2 "$CONFIG_PREFIX/conf.sh" conf.sh
--- build/scripts/run_schema_suggestor.sh
+++ build/scripts/run_schema_suggestor.sh
@@ -5,7 +5,6 @@ CONFIG_DIR="/opt/science/blueprint/config"
 SCIENCE_DIR="/opt/science"
 
 cd -- "$(dirname -- "$0")"
-eval "$(curl --silent 169.254.169.254/latest/user-data/)"
 export HOST="$(curl --silent 169.254.169.254/latest/meta-data/hostname)"
 export CONFIG_PREFIX="s3://$S3_CONFIG_BUCKET/$VPC_SUBNET_TAG/$CLOUD_APP/$CLOUD_ENVIRONMENT"
 export AWS_REGION=us-west-2
