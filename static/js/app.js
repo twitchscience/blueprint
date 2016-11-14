twitchscience/blueprint

commit eef0ff97fedbe804ebe3a3e9a2fe9be9b64c1633

Set sortkey by default
Test Plan: Tested locally

Reviewers: clgroft

Reviewed By: clgroft

Subscribers: mperrone, #scieng

Differential Revision: https://phabricator.internal.justin.tv/D4528

static/js/app.js
@@ -576,9 +576,11 @@ angular.module('blueprint', ['ngResource', 'ngRoute', 'ngCookies'])
             store.setError("At least one column is invalid; look at '" + item.InboundName + "'", undefined);
             return false;
           }
-          item.ColumnCreationOptions = '';
+          if (!item.ColumnCreationOptions) {
+            item.ColumnCreationOptions = '';
+          }
           if (item.Transformer === 'varchar') {
-            item.ColumnCreationOptions += '(' + item.size + ')';
+            item.ColumnCreationOptions = '(' + item.size + ')';
           }
           if (setDistKey == item.OutboundName) {
             item.ColumnCreationOptions += ' distkey';

commit 311b6eaaa96f4e0df1ec047e3c32c815fb0479e0

Fix bug setting sizes on non-varchar columns
Summary:
Bug introduced in eef0ff97fedbe804ebe3a3e9a2fe9be9b64c1633
ColumnCreationOptions was being set on schema creation suggestions,
which propagated to the column even when changed away from a varchar.

Test Plan: Ran locally

Reviewers: ojcm

Reviewed By: ojcm

Subscribers: #scieng

Differential Revision: https://phabricator.internal.justin.tv/D4578

static/js/app.js
@@ -535,6 +535,7 @@ angular.module('blueprint', ['ngResource', 'ngRoute', 'ngCookies'])
             if (match) {
               col.size = parseInt(match[1]);
             }
+            col.ColumnCreationOptions = '';
           }
           if (col.InboundName == 'device_id') {
             event.distkey = 'device_id';