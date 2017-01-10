6d62e4afcd06fbab3ab5c5f9fc4f0fa69821d672
Chad Groft <clgroft@twitch.tv>
2017-01-10 15:56:54 -0800

    Add maintenance mode table
    Summary:
    This table will track when maintenance mode is turned on or off, as well as
    its current state (the query for that will look at the maximum timestamp,
    hence the index).
    
    Test Plan:
    Ran the integration test, then looked in the Blueprint DB and verified that
    the correct table and row was present.
    
    Reviewers: jackgao
    
    Reviewed By: jackgao
    
    Subscribers: #scieng
    
    Differential Revision: https://phabricator.internal.justin.tv/D4839

--- init/init.sql
+++ init/init.sql
@@ -4,6 +4,7 @@ BEGIN
     CREATE TYPE action AS ENUM ('add', 'delete', 'rename', 'request_drop_event', 'drop_event', 'cancel_drop_event');
   END IF;
 END $$;
+
 CREATE TABLE IF NOT EXISTS operation
 (
   event varchar,
@@ -16,3 +17,18 @@ CREATE TABLE IF NOT EXISTS operation
   user_name varchar, -- will be 'legacy' for operations applied before this column existed. 'unknown' if user auth was disabled (like integration)
   PRIMARY KEY (event, version, ordering)
 );
+
+DO $$
+BEGIN
+  IF NOT EXISTS (SELECT 1 FROM pg_tables WHERE tablename = 'maintenance') THEN
+    CREATE TABLE maintenance
+    (
+      ts timestamp without time zone default NOW(),
+      is_maintenance boolean,
+      reason varchar
+    );
+    CREATE INDEX maintenance_ts_index ON maintenance(ts);
+    INSERT INTO maintenance (is_maintenance, reason) VALUES
+      (false, 'initializing table');
+  END IF;
+END $$;
