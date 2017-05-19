b02a5a8680e1994e60ba889ae3f4c624b3c577e1
Fred Cao <fredcao@justin.tv>
2017-05-19 16:16:37 -0700

    Added stats link to navbar
    Summary:
      - Added stats link to navbar
      - Rewrote button display logic to work correctly with new /stats link
    
    Test Plan: Check the new stats button takes you to /stats and the rest of the navbars work
    
    Reviewers: clgroft
    
    Reviewed By: clgroft
    
    Subscribers: #scieng
    
    Differential Revision: https://phabricator.internal.justin.tv/D5310

--- static/js/header.js
+++ static/js/header.js
@@ -1,16 +1,27 @@
 angular.module('blueprint')
   .controller('Header', function($scope, $location, store, auth) {
+    $scope.navAreas = {
+        'STATS':    'stats',         // /stats
+        'SCHEMA':   'schema',        // /schema
+        'KINESIS':  'kinesisconfig'  // /kinesisconfigs
+    };
     $scope.getError = store.getError;
     $scope.clearError = store.clearError;
     $scope.getMessage = store.getMessage;
     $scope.clearMessage = store.clearMessage;
     $scope.loginName = auth.getLoginName();
-    $scope.loc = $location;
-    $scope.currentEditor = 'schema';
-    $scope.currentEditor = $location.$$url.indexOf('/schema') == 0 ? 'schema' : $scope.currentEditor
-    $scope.currentEditor = $location.$$url.indexOf('/kinesisconfig') == 0 ? 'kinesisconfig' : $scope.currentEditor
-    $scope.updateEditor = function(newEditor) {
-      $scope.currentEditor = newEditor;
+    $scope.urlLocation = $location.$$url;
+    $scope.currentNavArea = (function(url) {
+        if (url.indexOf('/stats') == 0) {
+            return $scope.navAreas.STATS;
+        } else if (url.indexOf('/kinesisconfig') == 0) {
+            return $scope.navAreas.KINESIS;
+        } else {
+            return $scope.navAreas.SCHEMA;
+        }
+    })($scope.urlLocation);
+    $scope.updateNavArea = function(newNavArea) {
+      $scope.currentNavArea = newNavArea;
     };
     auth.isEditable($scope);
   });
